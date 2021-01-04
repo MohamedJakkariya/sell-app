1da52f031be5204935671405c87f59ab1b220736
MohamedJakkariya <mohamed.jakkariya@fleksa.com>
2021-01-04 12:32:26 +0530

    updated sql shema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,5 +1,10 @@
 -- Table names 
 1. Products - (id, name, label, labelColor, createdAt, amount)
-2. Sells
-3. Shops
-4. ShopAddress
+
+2. Sells - (id, skus, amount, description, createdAt, isPaid, due)
+
+3. SellProductList - (id, productId, )
+
+4. Shops - 
+
+5. ShopAddress
